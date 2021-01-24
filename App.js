b13f472494d6842f6e69a0cd0fe75d7630e830f1
abayram <38274063+abdullahbayram@users.noreply.github.com>
2021-01-24 14:10:09 +0300

    ResilientAutoHeightWebView in a ScrollView

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import EmptyScreen from "./EmptyScreen";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <EmptyScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
